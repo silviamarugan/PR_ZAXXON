e863c27a37b6fa6d287dbcdd8d4323df6b063d77
silviamarugan <silvia.mb2912@gmail.com>
2021-09-29 10:08:50 +0200

    CÁMARA resuelta
    Posición de la cámara definitiva. El eje X no funcionaba en la versión anterior. He puesto un Lens Shift en el proyecto para encuadrar mejor la cámara.

--- PR_ZAXXON_MaruganSilvia/Assets/Scripts/POS_Cam.cs
+++ PR_ZAXXON_MaruganSilvia/Assets/Scripts/POS_Cam.cs
@@ -5,12 +5,20 @@ using UnityEngine;
 public class POS_Cam : MonoBehaviour
 {
     // Start is called before the first frame update
-    [SerializeField] Transform PlayerPOS;
-    [SerializeField] float DistanciaPLayer = 10f;
-    [SerializeField] float AlturaCAM = 10f;
 
-    Vector3 CamPOS;
+    //VARIABLES ANTIGUAS
 
+    /*  [SerializeField] Transform PlayerPOS;
+      [SerializeField] float DistanciaPLayer = 10f;
+      [SerializeField] float AlturaCAM = 5f; 
+       Vector3 CamPOS; */
+
+    //FIN VARIABLES ANTIGUAS
+
+    [SerializeField] Transform playerPos;
+    float AlturaCAM = 3f;
+    [SerializeField] float smoothVelocity = 0.3F;
+    [SerializeField] Vector3 camaraVelocity = Vector3.zero;
 
     // Start is called before the first frame update
     void Start()
@@ -21,12 +29,22 @@ public class POS_Cam : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //CODIGO ANTIGUO
 
-        float newPosY = PlayerPOS.position.y + AlturaCAM;
-        float newPosZ = PlayerPOS.position.z - DistanciaPLayer;
-       
-      
-        transform.position = CamPOS = new Vector3(0f, newPosY, newPosZ);
+        /* float newPosY = PlayerPOS.position.y + AlturaCAM;
+       float newPosZ = PlayerPOS.position.z - DistanciaPLayer;
+
+
+       transform.position = CamPOS = new Vector3(0f, newPosY, newPosZ); */
+        //FIN CODIGO ANTIGUO
+
+        //SEGUIMIENTO CAMARA
+        float newPosY = playerPos.position.y + AlturaCAM;
+        transform.position = new Vector3(playerPos.position.x, newPosY, playerPos.position.z - 10);
+        // FIN SEGUIMIENTO CAMARA
+
+        Vector3 targetPosition = new Vector3(transform.position.x, playerPos.position.y, transform.position.z);
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref camaraVelocity, smoothVelocity);
     }
 
 }
