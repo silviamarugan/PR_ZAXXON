1aa8d012d96fca1a4d19c96f754eb532cbb3904a
silviamarugan <silvia.mb2912@gmail.com>
2021-10-06 10:15:28 +0200

    Rotacion obstaculos

--- PR_ZAXXON_MaruganSilvia/Assets/Scripts/Obstacles_INST.cs
+++ PR_ZAXXON_MaruganSilvia/Assets/Scripts/Obstacles_INST.cs
@@ -28,13 +28,13 @@ public class Obstacles_INST : MonoBehaviour
         while (true)
         {
 
-          
+            float rotationVAL = Random.Range(0f, 360f); //rotacion de las columnas 
             Vector3 instPOS = new Vector3(Random.Range(-70f, 70f), Random.Range(5f, 30f), initPOS.position.z);
 
-            
 
-            Instantiate(Columns, instPOS , Quaternion.identity);
 
+            Instantiate(Columns, instPOS, Quaternion.Euler(0f,0f, rotationVAL)); //en caso de querer columnas fijas usar -> Quaternion.identity
+          
 
             // Quaternion.Euler(Random.Range(0.0f, 360.0f), Random.Range(0.0f, 360.0f), Random.Range(0.0f, 360.0f)) para rotar las columnas. 
             yield return new WaitForSeconds(intervalo);
