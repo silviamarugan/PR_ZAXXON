5ff1e029e76beb13622b4234a5d0f52e4d1e0b1f
Silvia <silvia.mb2912@gmail.com>
2021-10-27 10:22:40 +0200

    Cambios minimos

--- PR_ZAXXON_MaruganSilvia/Assets/Scripts/MOV_Cols.cs
+++ PR_ZAXXON_MaruganSilvia/Assets/Scripts/MOV_Cols.cs
@@ -23,7 +23,20 @@ public class MOV_Cols : MonoBehaviour
 
         transform.Translate(Vector3.back * Time.deltaTime * speed);
         float posZ = transform.position.z;
-   
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            bool slow = false;
+            //  rend.material.shader = Shader.Find("Highlight");
+            //  rend.material.SetColor("Highlight", Color.blue);
+
+            slow = true;
+            print("Slow");
+            if (slow == true)
+            {
+                speed = 8f;
+
+            }
+        }
 
         if (posZ < -40)
         {
